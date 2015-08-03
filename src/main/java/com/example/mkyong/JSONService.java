7402cc7ace3d1382fc469c7a91c3f96d1c3f6fef
albertocerqueira <alberto.cerqueira1990@gmail.com>
2015-08-03 09:13:56 -0300

    Commit initial

---
+++ src/main/java/com/example/mkyong/JSONService.java
@@ -0,0 +1,40 @@
+package com.example.mkyong;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/json/metallica")
+public class JSONService {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+		return track;
+	}
+	
+	// added by Alberto Cerqueira for testing
+	@GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String music) {
+        String output = "Music : " + music;
+        return Response.status(200).entity(output).build();
+    }
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+		String result = "Track saved : " + track;
+		return Response.status(201).entity(result).build();
+	}
+}
